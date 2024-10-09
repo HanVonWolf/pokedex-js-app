4f2a7dc8c9067fd3e44065a2f221edf58402ee19
HanVonWolf <hannahwolfenden@outlook.com>
2024-10-09 11:31:28 +0200

    Added pokemonList Array to Scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name:'Quaigsire', height: 1.4, type: ['water', 'ground']},
+    {name: 'Phanpy', height: 0.5, type: ['ground']},
+    {name: 'Bidoof', height:0.5, type: ['normal'] },
+];
