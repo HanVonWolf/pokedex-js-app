0a64bc0281f2e1ffabd1ba80e0c1c21534f67f65
HanVonWolf <hannahwolfenden@outlook.com>
2024-10-16 12:25:00 +0200

    Addition of for loop on script.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ let pokemonList = [
     {name: 'Phanpy', height: 0.5, type: ['ground']},
     {name: 'Bidoof', height:0.5, type: ['normal'] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    console.log('name:', pokemon.name);
+    console.log('height:', pokemon.height);
+    document.write('<p>' + pokemon.name + ' | Height: ' + pokemon.height + '</p>');
+    if (pokemon.height >= 1) {
+        document.write('Wow, that is a big Pokemon!');
+    }
+}
