eab1476046bec85ffeefb91665e72f2087faac7b
HanVonWolf <hannahwolfenden@outlook.com>
2024-10-28 11:24:07 +0100

    Addition of forEach() loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,31 @@ let pokemonList = [
     {name: 'Bidoof', height:0.5, type: ['normal'] },
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    let pokemon = pokemonList[i];
+pokemonList.forEach(function(pokemon) {
     console.log('name:', pokemon.name);
     console.log('height:', pokemon.height);
     document.write('<p>' + pokemon.name + ' | Height: ' + pokemon.height + '</p>');
     if (pokemon.height >= 1) {
         document.write('Wow, that is a big Pokemon!');
     }
+})
+
+/*for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    console.log('name:', pokemon.name);
+    console.log('height:', pokemon.height);
+    document.write('<p>' + pokemon.name + ' | Height: ' + pokemon.height + '</p>');
+    if (pokemon.height >= 1) {
+        document.write('Wow, that is a big Pokemon!');
+    } 
+} */
+//Previous loop but improved with a forEach loop
+
+function divide (dividend, divisor) {
+    if(divisor === 0) {
+        return "You're trying to divide by zero"
+}else{
+    let result = dividend / divisor;
+    return result;
+}
 }
