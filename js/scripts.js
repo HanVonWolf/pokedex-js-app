4c11c0f165d4ae9a142df9b09a430a5be8eda595
HanVonWolf <hannahwolfenden@outlook.com>
2024-11-21 12:57:41 +0100

    Order of JS code

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+function add(pokemon) {
+      pokemonList.push(pokemon);
+  }
+
 function getAll() {
     return pokemonList;
 }
